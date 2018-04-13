b05d313880923f8217d552ca5d444a3f5507bcf6
Jitendra Zaa <jitendra.zaaibm.com@Jitendras-MacBook-Pro.local>
2018-04-12 22:03:05 -0400

    Added new OAuth Flow

--- Server.js
+++ Server.js
@@ -12,6 +12,7 @@ var express = require('express'),
 	apiVersion = 'v38.0',
 	domainName='localhost:8081',
 	jwt_consumer_key = '3MVG9szVa2RxsqBYmpkiG1QgjELvD9Z0NRvySRZKne.sBmUyKr9jPLaKWxAlFr3vvGGUGQskU3c.QPQyQuOel', 
+	consumer_secret='4390324792281178734',
 	jwt_aud = 'https://login.salesforce.com', 
 	callbackURL='https://localhost:8081/oauthcallback.html';
 
@@ -22,6 +23,30 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());  
 
 app.set('port', process.env.PORT || 8080);
+
+/**
+ *  Extract Access token from POST response and redirect to page Main
+ */
+function extractAccessToken(err, remoteResponse, remoteBody,res){
+	if (err) { 
+		return res.status(500).end('Error'); 
+	}
+	console.log(remoteBody) ;
+	var sfdcResponse = JSON.parse(remoteBody); 
+	
+	//success
+	if(sfdcResponse.access_token){				 
+		res.writeHead(302, {
+		  'Location': 'Main' ,
+		  'Set-Cookie': ['AccToken='+sfdcResponse.access_token,'APIVer='+apiVersion,'InstURL='+sfdcResponse.instance_url,'idURL='+sfdcResponse.id]
+		});
+	}else{
+		res.write('Some error occurred. Make sure connected app is approved previously. ');
+		res.write('Salesforce Response : ');
+		res.write( remoteBody ); 
+	} 
+	res.end();
+}
 
 app.all('/proxy',  function(req, res) {     
     var url = req.header('SalesforceProxy-Endpoint');  
@@ -47,34 +72,61 @@ app.get('/jwt', function (req,res){
 				
 	request(req_sfdcOpts, 
 		function(err, remoteResponse, remoteBody) {
-			if (err) { 
-				return res.status(500).end('Error'); 
-			}
-			console.log(remoteBody) ;
-			var sfdcResponse = JSON.parse(remoteBody); 
-			
-			//success
-			if(sfdcResponse.access_token){				 
-				res.writeHead(302, {
-				  'Location': 'Main' ,
-				  'Set-Cookie': ['AccToken='+sfdcResponse.access_token,'APIVer='+apiVersion,'InstURL='+sfdcResponse.instance_url,'idURL='+sfdcResponse.id]
-				});
-			}else{
-				res.write('Some error occurred. Make sure connected app is approved previously. ');
-				res.write('Salesforce Response : ');
-				res.write( remoteBody ); 
-			} 
-			res.end();
-
+			extractAccessToken(err, remoteResponse, remoteBody, res); 
 		} 
 	); 
 } );
 
+/**
+ * Step 1 Web Server Flow - Get Code
+ */
+app.get('/webServer', function (req,res){  
+	var isSandbox = req.query.isSandbox;
+	var state = 'webServerProd';
+	var sfdcURL = 'https://login.salesforce.com/services/oauth2/authorize' ;
+	if(isSandbox == 'true'){
+		sfdcURL = 'https://test.salesforce.com/services/oauth2/authorize' ;
+		state = 'webServerSandbox';
+	}
+	
+	 request({ 	url : sfdcURL+'?client_id='+
+				 jwt_consumer_key+'&redirect_uri='+
+				 callbackURL+'&response_type=code&state='+state,  
+				method:'GET' 
+			}).pipe(res);
+	 
+} );
+
+
 
 /**
-*	Authenticate first time for JWT using User Agent flow
+ * Step 2 Web Server Flow - Get token from Code
+ */
+app.get('/webServerStep2', function (req,res){  
+	var state = req.query.state;
+	var sfdcURL = 'https://login.salesforce.com/services/oauth2/token' ;
+	if(state == 'webServerSandbox'){
+		sfdcURL = 'https://test.salesforce.com/services/oauth2/token' ;
+	}
+	
+	 request({ 	url : sfdcURL+'?client_id='+
+				 jwt_consumer_key+'&redirect_uri='+
+				 callbackURL+'&grant_type=authorization_code&code='+
+				 req.query.code+'&client_secret'+consumer_secret,  
+				method:'POST' 
+			},
+			function(err, remoteResponse, remoteBody) {
+				extractAccessToken(err, remoteResponse, remoteBody, res); 
+			} 
+		);
+	 
+} );
+
+
+/**
+*	 User Agent flow oAuth Flow
 */
-app.get('/jwtF', function (req,res){  
+app.get('/uAgent', function (req,res){  
 	var isSandbox = req.query.isSandbox;
 	var sfdcURL = 'https://login.salesforce.com/services/oauth2/authorize' ;
 	if(isSandbox == 'true'){
